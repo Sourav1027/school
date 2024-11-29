8b4569fbe1605ca29fd60a2b464fe070273b222a
Sourav Rana <92842863+Sourav1027@users.noreply.github.com>
2024-11-29 13:03:30 +0530

    Delete Dashcode-v1.0.0 directory

--- Dashcode-v1.0.0/components/partials/auth/copyright.tsx
+++
@@ -1,8 +0,0 @@
-
-
-const Copyright = () => {
-    const currentYear = new Date().getFullYear();
-  return <>Copyright {currentYear}, schoolUp All Rights Reserved.</>;
-};
-
-export default Copyright;
