2b53dba50f132cc3d2bccf072e4abb5e6299c78c
Sourav1027 <ranasourav57.sr@gmail.com>
2024-11-29 16:32:23 +0530

    card import

--- Dashcode/app/[locale]/(protected)/components/blocks/progress-block.tsx
+++ Dashcode/app/[locale]/(protected)/components/blocks/progress-block.tsx
@@ -3,7 +3,7 @@ import dynamic from "next/dynamic";
 const Chart = dynamic(() => import("react-apexcharts"), { ssr: false });
 import { cn } from "@/lib/utils";
 import { useTheme } from "next-themes";
-import { Card, CardContent } from "../ui/card";
+import { Card, CardContent } from '@/components/ui/card';
 
 interface ProgressBlockProps {
   title?: string,
