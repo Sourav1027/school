096e973711b1f56a62589348cd23414c88d439b2
Sourav1027 <ranasourav57.sr@gmail.com>
2024-12-16 10:39:13 +0530

    change

--- Dashcode/components/partials/auth/login-form.tsx
+++ Dashcode/components/partials/auth/login-form.tsx
@@ -65,7 +65,7 @@ const LoginForm: React.FC = () => {
         return;
       }
       
-      const token = result.token;
+      const token = result.jwt;
       if (!token) {
         throw new Error("Token is missing from the API response");
       }
